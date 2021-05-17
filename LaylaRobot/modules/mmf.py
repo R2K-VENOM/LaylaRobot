7a79eef81c57710e71e8f95a9ad7dee839c3ebc1
RYNO-X <73129419+RYNO-X@users.noreply.github.com>
2021-05-17 13:40:29 +0530

    Add files via upload

---
+++ LaylaRobot/modules/mmf.py
@@ -0,0 +1,213 @@
+ # zombies Module Developed and Provided by @uday_gondaliya 
+
+import asyncio
+from asyncio import sleep
+
+from telethon import events
+from telethon.errors import ChatAdminRequiredError, UserAdminInvalidError
+from telethon.tl.functions.channels import EditBannedRequest
+from telethon.tl.types import ChatBannedRights, ChannelParticipantsAdmins
+
+from LaylaRobot import telethn, OWNER_ID, DEV_USERS, DRAGONS, DEMONS
+
+# =================== CONSTANT ==================
+import os
+import textwrap
+
+import cv2
+from PIL import Image, ImageDraw, ImageFont
+
+from . import *
+
+BANNED_RIGHTS = ChatBannedRights(
+    until_date=None,
+    view_messages=True,
+    send_messages=True,
+    send_media=True,
+    send_stickers=True,
+    send_gifs=True,
+    send_games=True,
+    send_inline=True,
+    embed_links=True,
+)
+
+
+UNBAN_RIGHTS = ChatBannedRights(
+    until_date=None,
+    send_messages=None,
+    send_media=None,
+    send_stickers=None,
+    send_gifs=None,
+    send_games=None,
+    send_inline=None,
+    embed_links=None,
+)
+
+OFFICERS = [OWNER_ID] + DEV_USERS + DRAGONS + DEMONS
+
+# Check if user has admin rights
+async def is_administrator(user_id: int, message):
+    admin = False
+    async for user in telethn.iter_participants(
+        message.chat_id, filter=ChannelParticipantsAdmins
+    ):
+        if user_id == user.id or user_id in OFFICERS:
+            admin = True
+            break
+    return admin
+
+
+
+@telethn.on(events.NewMessage(pattern=f"^[!/]mmf ?(.*)"))
+async def mmf(event):
+    """ For .mmf command, to memify the sticker"""
+    ureply = update.effective_user
+    msg = update.effective_message
+    if not await is_administrator(user_id=event.from_id, message=event):
+        await event.respond("You're Not An Admin!")
+        return
+    if not admin and not creator:
+        await event.respond("I Am Not An Admin Here!")
+        return
+    ultt = await ureply.download_media()
+    if ultt.endswith((".tgs")):
+        xx = await event.respond( "`Ooo Animated Sticker 👀...`")
+        cmd = ["lottie_convert.py", ultt, "hunter.png"]
+        file = "hunter.png"
+        process = await asyncio.create_subprocess_exec(
+            *cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+        )
+        stdout, stderr = await process.communicate()
+        stderr.decode().strip()
+        stdout.decode().strip()
+    elif ultt.endswith((".webp", ".png")):
+        xx = await event.respond( "`Processing`")
+        im = Image.open(ultt)
+        im.save("hunter.png", format="PNG", optimize=True)
+        file = "hunter.png"
+    else:
+        xx = await event.respond( "`Processing`")
+        img = cv2.VideoCapture(ultt)
+        heh, lol = img.read()
+        cv2.imwrite("hunter.png", lol)
+        file = "hunter.png"
+    stick = await draw_meme_text(file, msg)
+    await ultroid_bot.send_file(
+        event.chat_id, stick, force_document=False, reply_to=event.reply_to_msg_id
+    )
+    await xx.delete()
+    try:
+        os.remove(ultt)
+        os.remove(file)
+        os.remove(stick)
+    except BaseException:
+        pass
+
+
+async def draw_meme_text(image_path, msg):
+    img = Image.open(image_path)
+    os.remove(image_path)
+    i_width, i_height = img.size
+    if "_" in msg:
+        text, font = msg.split("_")
+    else:
+        text = msg
+        font = "default"
+    if ";" in text:
+        upper_text, lower_text = text.split(";")
+    else:
+        upper_text = text
+        lower_text = ""
+    draw = ImageDraw.Draw(img)
+    m_font = ImageFont.truetype(
+        f"resources/fonts/{font}.ttf", int((70 / 640) * i_width)
+    )
+    current_h, pad = 10, 5
+    if upper_text:
+        for u_text in textwrap.wrap(upper_text, width=15):
+            u_width, u_height = draw.textsize(u_text, font=m_font)
+            draw.text(
+                xy=(((i_width - u_width) / 2) - 1, int((current_h / 640) * i_width)),
+                text=u_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=(((i_width - u_width) / 2) + 1, int((current_h / 640) * i_width)),
+                text=u_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=((i_width - u_width) / 2, int(((current_h / 640) * i_width)) - 1),
+                text=u_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=(((i_width - u_width) / 2), int(((current_h / 640) * i_width)) + 1),
+                text=u_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=((i_width - u_width) / 2, int((current_h / 640) * i_width)),
+                text=u_text,
+                font=m_font,
+                fill=(255, 255, 255),
+            )
+            current_h += u_height + pad
+    if lower_text:
+        for l_text in textwrap.wrap(lower_text, width=15):
+            u_width, u_height = draw.textsize(l_text, font=m_font)
+            draw.text(
+                xy=(
+                    ((i_width - u_width) / 2) - 1,
+                    i_height - u_height - int((80 / 640) * i_width),
+                ),
+                text=l_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=(
+                    ((i_width - u_width) / 2) + 1,
+                    i_height - u_height - int((80 / 640) * i_width),
+                ),
+                text=l_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=(
+                    (i_width - u_width) / 2,
+                    (i_height - u_height - int((80 / 640) * i_width)) - 1,
+                ),
+                text=l_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=(
+                    (i_width - u_width) / 2,
+                    (i_height - u_height - int((80 / 640) * i_width)) + 1,
+                ),
+                text=l_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=(
+                    (i_width - u_width) / 2,
+                    i_height - u_height - int((80 / 640) * i_width),
+                ),
+                text=l_text,
+                font=m_font,
+                fill=(255, 255, 255),
+            )
+            current_h += u_height + pad
+    imag = "ultt.webp"
+    img.save(imag, "WebP")
+    return imag
+
+
