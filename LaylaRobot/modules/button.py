4320f78291ee403a67e763b0878d07d109986187
R2K-VENOM <84581636+R2K-VENOM@users.noreply.github.com>
2021-07-11 15:22:20 +0530

    Create button.py

---
+++ LaylaRobot/modules/button.py
@@ -0,0 +1,13 @@
+from telethon import events, Button, client
+from LaylaRobot import DEV_USERS
+
+@BotzHub.on(
+    events.NewMessage(pattern="^/skem ?(.*)")
+)
+async def _(event):
+  if event.sender_id in DEV_USERS:
+      text = event.pattern_match.group(1)
+      k = [[Button.text(text)]]
+      await BotzHub.send_message(event.chat_id, "😈", buttons=k)
+  else:
+      await event.reply("**BHAI YAAR THUM GAAND MARAO**")
